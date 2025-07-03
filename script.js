b7e450c0bcb1185affa88de312ef3e5c7c76841b
moisescaris <moises.caris@edu.unifil.br>
2025-07-03 19:27:19 -0300

    ATV-18 feat:Crud usuarios

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let users = [];
+let users = []
 
 function addUser() {
-  const name = document.getElementById("userName").value.trim();
-  const email = document.getElementById("userEmail").value.trim();
+  const name = document.getElementById("userName").value.trim()
+  const email = document.getElementById("userEmail").value.trim()
   if (name && email) {
-    users.push({ name, email });
+    users.push({ name, email })
     renderUsers();
-    document.getElementById("userName").value = "";
-    document.getElementById("userEmail").value = "";
+    document.getElementById("userName").value = ""
+    document.getElementById("userEmail").value = ""
   }
 }
